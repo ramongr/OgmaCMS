9828d6b36ecb995ef65ea7e894e8bc36c2716006
Rui Sá <megajpc@gmail.com>
2014-07-23 15:33:07 +0100

    Forem configuration finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+//= require turbolinks
