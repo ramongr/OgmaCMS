d32bcdcbe178d24c2f6ba0fe06e3e00198f2eb66
Nelson Brandão <nelsonbrandao800@gmail.com>
2014-08-13 13:45:01 +0100

    Added confirm to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,25 +12,25 @@
 puts "Adding users"
 
 if User.where(email: 'super_admin@example.com').empty?
-  super_admin = User.new name: 'Super Admin', email: 'super_admin@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'true', role: 'super_admin'
+  super_admin = User.new name: 'Super Admin', email: 'super_admin@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'true', role: 'super_admin', confirmed_at: Time.now
   super_admin.save!
   puts "Created User Super Admin, super_admin@example.com"
 end
 
 if User.where(email: 'admin@example.com').empty?
-  admin = User.new name: 'Admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'true', role: 'admin'
+  admin = User.new name: 'Admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'true', role: 'admin', confirmed_at: Time.now
   admin.save!
   puts "Created User Admin, admin@example.com"
 end
 
 if User.where(email: 'author@example.com').empty?
-  author = User.new name: 'Author', email: 'author@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'false', role: 'author'
+  author = User.new name: 'Author', email: 'author@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'false', role: 'author', confirmed_at: Time.now
   author.save!
   puts "Created User Author, author@example.com"
 end
 
 if User.where(email: 'registered@example.com').empty?
-  registered = User.new name: 'Registered', email: 'registered@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'false', role: 'registered'
+  registered = User.new name: 'Registered', email: 'registered@example.com', password: 'password', password_confirmation: 'password', forem_admin: 'false', role: 'registered', confirmed_at: Time.now
   registered.save!
   puts "Created User Registered, registered@example.com"
 end
