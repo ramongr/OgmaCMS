feb600842316b1edb6025cb4958f84c056b3eac7
Bolturius <redbolt@gmail.com>
2014-08-28 16:57:23 +0100

    fix params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
 
   def forem_user
     current_user
