6394496070203a66ae35137a1bde950d434dbadd
banned <pjmaRquez888@gmail.com>
2014-08-28 22:55:55 +0100

    Create and Send Newsletters

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,4 @@
 class Admin::EventsController < Admin::AdminController
-  helper :all #ApplicationHelper
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
 
