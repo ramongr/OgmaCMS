31b2f5b528f3abadbb980ed72234f69dacaefd79
banned <pjmaRquez888@gmail.com>
2014-09-17 23:59:55 +0100

    Select Main Slider

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,4 +1,9 @@
 class Slider < ActiveRecord::Base
   validates :title, presence: true
   has_many :photos, as: :imageable, dependent: :destroy
+
+  def self.selected
+    Slider.find_by selected: true
+  end
+
 end
