2b2fb2461aceb3a4ae67b24ff010e823ab5da363
Hugo Granja <redbolt@gmail.com>
2014-09-26 19:38:02 +0100

    minor improvements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902214225) do
+ActiveRecord::Schema.define(version: 20140926174241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,7 +158,7 @@ ActiveRecord::Schema.define(version: 20140902214225) do
   create_table "galleries", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "date"
+    t.date     "date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
