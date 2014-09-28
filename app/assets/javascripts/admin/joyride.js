c6b64d99e4763244415c34584fb1540aa97a60c1
banned <pjmaRquez888@gmail.com>
2014-09-29 00:26:16 +0100

    JoyRide and Minor Fixes

--- app/assets/javascripts/admin/joyride.js
+++ app/assets/javascripts/admin/joyride.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   if($.cookie(cookie_id) === null)
   { 
     //Expires in 10000 days
-    $.cookie(cookie_id, '1', { expires: 10000 });
+    $.cookie(cookie_id, '1', { expires: 10000, path: '/'});
   }
   else
   {
