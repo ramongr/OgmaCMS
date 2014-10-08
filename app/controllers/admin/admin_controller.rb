4c7416da59122474939df4a94a962a233150d899
Bolturius <redbolt@gmail.com>
2014-10-08 22:38:15 +0100

    better webpage titles

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,6 @@
 class Admin::AdminController < ApplicationController
   before_filter :authenticate_user!
   before_filter :verify_admin
-  before_action :set_username_cookie, only: [:new, :edit]
 
   layout 'admin'
 
@@ -9,11 +8,5 @@ class Admin::AdminController < ApplicationController
 
   def verify_admin
     redirect_to root_url, notice: 'Not authorized' unless current_user.role? :admin
-  end
-
-  def set_username_cookie
-    if current_user
-      cookies[:user_name] = current_user.email
-    end
   end
 end
