e814516587fa7aa6684c7a73131ee452cbec9a4c
Hugo Granja <redbolt@gmail.com>
2014-10-24 18:04:33 +0100

    internationalize show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
 
   def user_language
     unless current_user.try(:language).nil?
-      current_user.language.to_sym
+      if Setting.selected_languages.include?(current_user.language)
+        current_user.language.to_sym
+      else
+        ulang = http_accept_language.compatible_language_from(Setting.selected_languages)
+        current_user.update_attribute(:language,ulang)
+        ulang
+      end
     else
       http_accept_language.compatible_language_from(Setting.selected_languages)
     end
@@ -11,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   def set_i18n_locale
     if Setting.selected_languages.include?(params[:locale])
-      if params[:locale] != I18n.locale
+      if params[:locale] != I18n.locale.to_s
         I18n.locale = params[:locale]
         if user_signed_in?
           current_user.update_attribute(:language,I18n.locale.to_s)
