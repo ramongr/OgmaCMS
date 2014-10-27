811e5f9043b186cf7ee37d20cec3d3de32f11c65
Ramon Rodrigues <cerberus.ramon@gmail.com>
2014-10-27 19:01:44 +0000

    update

--- app/assets/javascripts/ckeditor/plugins/mediabutton/plugin.js
+++ app/assets/javascripts/ckeditor/plugins/mediabutton/plugin.js
@@ -1,12 +1,15 @@
 CKEDITOR.plugins.add('mediabutton', {
   icons: 'mediabutton',
   init: function( editor ){
+    
+    editor.addCommand('mediabutton',{
+      exec: document.getElementById('insert-attachment').click()
+    })
+
     editor.ui.addButton( 'Mediabutton',{
       label: 'Insert Media',
       command: 'mediabutton',
       toolbar: 'insert'
     });
-
-    $('#insert-attachment').click()
   }
 })
